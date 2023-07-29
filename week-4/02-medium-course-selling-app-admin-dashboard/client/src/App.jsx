09d8637b869a5b36beff1b639f82140e2470f1a8
Somnath Golui <code.somu@gmail.com>
2023-07-29 18:51:40 +0530

    Added PageNotFound page

--- week-4/02-medium-course-selling-app-admin-dashboard/client/src/App.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="signin" element={<Signin />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
   );

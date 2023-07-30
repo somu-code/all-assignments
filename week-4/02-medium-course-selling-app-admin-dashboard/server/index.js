807ef7e9682d2b973caf299631f1ad9310fbad39
Somnath Golui <code.somu@gmail.com>
2023-07-30 09:41:40 +0530

    express route working properly

--- week-4/02-medium-course-selling-app-admin-dashboard/server/index.js
+++ week-4/02-medium-course-selling-app-admin-dashboard/server/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = 3000;
 
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
+
 app.post("/admin/signup", (req, res) => {
-  console.log(res);
-  res.sendStatus(200);
+  res.status(200).json({ message: "Response" });
 });
 
 app.listen(3000, () => {

f3b1a4b7cb3f8da04d4699f11997adc8b19a33cd
Somnath Golui <code.somu@gmail.com>
2023-07-31 02:36:03 +0530

    Created AddCourse page. Coded the UI. State managment still left.

--- week-4/02-medium-course-selling-app-admin-dashboard/client/src/App.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/client/src/App.jsx
@@ -4,6 +4,7 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
+import AddCourse from "./pages/AddCourse";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
+        <Route path="/add-course" element={<AddCourse />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
---
+++ week-4/02-medium-course-selling-app-admin-dashboard/client/src/pages/AddCourse.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+function AddCourse() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col bg-slate-300 p-6 gap-4 rounded-xl w-2/5"
+      >
+        <h3 className="text-center font-semibold text-[#2866df]">Add course</h3>
+        <input
+          type="text"
+          name="title"
+          id="courseTitle"
+          placeholder="Title"
+          className="pl-2 py-2 rounded-md focus:outline-blue-500"
+          onChange={(event) => setTitle(event.target.value)}
+          value={title}
+        />
+        <input
+          type="text"
+          name="description"
+          id="courseDescription"
+          placeholder="Description"
+          className="pl-2 py-2 rounded-md focus:outline-blue-500"
+          onChange={(event) => setDescription(event.target.value)}
+          value={description}
+        />
+        <input
+          type="number"
+          name="price"
+          id="coursePrice"
+          placeholder="Price"
+          className="pl-2 py-2 rounded-md focus:outline-blue-500"
+          onChange={(event) => setPrice(event.target.value)}
+        />
+        <div className="bg-white py-2 rounded-md px-2 flex flex-row justify-between items-center">
+          <label htmlFor="coursePublished">Published</label>
+          <select name="published" id="coursePublished">
+            <option value="true">True</option>
+            <option value="false">False</option>
+          </select>
+        </div>
+        <div className="bg-white py-2 rounded-md px-2 flex flex-row justify-between items-center">
+          <label htmlFor="courseImage">
+            Course Thumbnail (maximum file size 15MB)
+          </label>
+          <input
+            type="file"
+            name="image"
+            id="courseImage"
+            accept="image/*"
+            capture="user"
+            className="pl-4"
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-[#2866df] text-white font-semibold py-2 rounded-md hover:bg-[#215ac8]"
+        >
+          Add course
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default AddCourse;
